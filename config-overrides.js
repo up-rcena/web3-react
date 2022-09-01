8ae9429590865a65f3173349d245232fa897e2e1
startechup.roms <startechup-roms@users.noreply.github.com>
2022-09-02 03:30:16 +0800

    - initial commit

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+const webpack =  require('webpack')
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    stream: require.resolve('stream-browserify'),
+    crypto: require.resolve('crypto-browserify'),
+    http: require.resolve('stream-http'),
+    https: require.resolve('https-browserify'),
+    os: require.resolve('os-browserify/browser')
+  };
+
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer']
+    })
+  )
+
+  return config;
+}
